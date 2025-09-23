808d48c4cc084a8ec4337d77bf68ae8866e6cd0c
23070051-beep <23070051@vnu.edu.vn>
2025-09-23 14:25:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,14 @@
     $course = array("JAVA", "PHP", "HTML", "CSS", "JavaScript");
     echo     "Name:" . $name . ", age:" . $age .
              "<br>3rd courses is " . $course[2];
+    $x = 10;
+    $y = 11;
+    echo "x: " . $x . "<br>";
+    echo "y: " . $y . "<br>";
+    echo "x/y: " . ($x/$y) . "<br>";
+    echo "x%y: " . ($x%$y) . "<br>";
+    echo "x++: " . ($x++) . "<br>";
+    echo "++y: " . (++$y) . "<br>";
     ?>
 </body>
 </html>

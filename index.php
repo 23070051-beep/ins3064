4fff14cbe8f304dba3222c42cdf97b5430d68240
23070051-beep <23070051@vnu.edu.vn>
2025-09-23 15:12:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
  <?php
     $x = 10;
+<!DocTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    $x = $_GET["x"];
+    $y = $_GET["y"];
     $y = 11;
     echo "x == y: " . ($x==$y) . "<br>";
+    echo "x: " . $x . "<br>";
+    echo "y: " . $y . "<br>";
+    echo "x+y: " . ($x+$y) . "<br>";
+    echo "x-y: " . ($x-$y) . "<br>";
+    echo "x*y: " . ($x*$y) . "<br>";
+    echo "x/y: " . ($x/$y) . "<br>";
+    echo "x%y: " . ($x%$y) . "<br>";
+    echo "x++: " . ($x++) . "<br>";
+    echo "++y: " . (++$y) . "<br>";
+    echo "x == y: " . ($x == $y) . "<br>";
     echo "x != y: " . ($x != y) . "<br>";
     echo "x < y: " . ($x < y) . "<br>";
     echo "x > y: " . ($x > $y) . "<br>";
     echo "x <= y: " . ($x <= $y) . "<br>";
-    echo "x >=: " . ($x >= $y) . "<br>";
+    echo "x >= y: " . ($x >= $y) . "<br>";
     ?>
+</body>
+</html>

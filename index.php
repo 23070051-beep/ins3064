6774fb8a6b8c5ba41088a5f137690f024bae1db8
23070051-beep <23070051@vnu.edu.vn>
2025-09-23 13:46:02 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DocTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    $name = "Dong";
+    $age = 20;
+    $course = array("JAVA", "PHP", "HTML", "CSS", "JavaScript");
+    echo     "Name:" . $name . ", age:" . $age .
+             "<br>3rd courses is " . $course[2];
+    ?>
+</body>
+</html>
